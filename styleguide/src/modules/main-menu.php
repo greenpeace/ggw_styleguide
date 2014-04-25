654c45c4a118a960da5ef2201bc17a0527bbfb04
MaikelGG <maikel.koopman@gmail.com>
2014-04-25 16:57:12 +0200

    change template file to php so we can include repeating elements, start navigation for desktop

---
+++ styleguide/src/modules/main-menu.php
@@ -0,0 +1,25 @@
+<ul class="menu">
+  <li class="menu-item"><a href="#" class="menu-link active">Home</a></li>
+  <li class="menu-item expanded"><a href="#" class="menu-link">Community</a>
+    <ul class="menu">
+      <li class="menu-item"><a href="#" class="menu-link">Events</a></li>
+      <li class="menu-item"><a href="#" class="menu-link">Groups</a></li>
+    </ul>
+  </li>
+  <li class="menu-item expanded"><a href="#" class="menu-link">News &amp; media</a>
+    <ul class="menu">
+      <li class="menu-item"><a href="#" class="menu-link">News</a></li>
+      <li class="menu-item"><a href="#" class="menu-link">Blogs</a></li>
+      <li class="menu-item"><a href="#" class="menu-link">Photos</a></li>
+      <li class="menu-item"><a href="#" class="menu-link">Videos</a></li>
+      <li class="menu-item"><a href="#" class="menu-link">Documents</a></li>
+      <li class="menu-item"><a href="#" class="menu-link">Ideas</a></li>
+    </ul>
+  </li>
+  <li class="menu-item expanded"><a href="#" class="menu-link">About</a>
+     <ul class="menu">
+      <li class="menu-item"><a href="#" class="menu-link">Contact</a></li>
+      <li class="menu-item"><a href="#" class="menu-link">FAQ</a></li>
+    </ul>
+  </li>
+</ul>
---
+++ styleguide/src/modules/search.php
@@ -0,0 +1,8 @@
+<form action="#" method="post" class="search-form" accept-charset="UTF-8">
+  <div class="form-item-search">
+     <input class="form-text" name="search" type="text" size="60" maxlength="128" placeholder="Search..." />
+  </div>
+  <div class="form-actions">
+    <input class="form-submit" type="submit" name="op" value="Search" />
+  </div>
+</form>

a7151bf6bf4bde14e387056c42b7b78fe8c129ae
MaikelGG <maikel.koopman@gmail.com>
2014-05-14 09:00:34 +0200

    move header part to separate file

---
+++ styleguide/src/modules/head.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--[if (IE 7)&(!IEMobile)]><html class="no-js ie lt-ie9 lt-ie8" lang="nl" dir="ltr"><![endif]-->
+<!--[if IE 8]><html class="no-js ie lt-ie9" lang="nl" dir="ltr"><![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><html class="no-js ie" lang="nl" dir="ltr"><![endif]-->
+<!--[if !IE]><!--><html class="no-js" lang="en" dir="ltr"><!--<![endif]-->
+<head>
+
+<title>Discover Greenwire | Greenpeace Greenwire Nederland</title>
+<meta charset="utf-8" />
+<meta name="HandheldFriendly" content="true" />
+<meta name="MobileOptimized" content="width" />
+<link rel="profile" href="http://www.w3.org/1999/xhtml/vocab" />
+<link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
+<meta name="Generator" content="Drupal 7 (http://drupal.org)" />
+<meta name="viewport" content="width=device-width" />
+
+<!--[if (gte IE 9)|(gt IEMobile 7)|(!IE)]><!-->
+<link rel="stylesheet" href="css/ggw.styles.css" />
+<!--<![endif]-->
+
+<!--[if (gte IE 6)&(lte IE 8)]>
+<link rel="stylesheet" href="css/ggw.no-query.css" />
+<script src="libraries/html5shiv/html5shiv.min.js"></script>
+<script src="libraries/html5shiv/html5shiv-print.min.js"></script>
+<![endif]-->
+
+<script src="js/theme.js"></script>
+
+</head>

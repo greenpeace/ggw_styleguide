7b3ee84048572a515d95f1777e02b85e582139f7
jlalovi <jlalovi@gmail.com>
2014-05-21 15:07:04 +0200

    GREEN-1656 footer for desktop php/sass

---
+++ styleguide/src/modules/footer.php
@@ -0,0 +1,9 @@
+<footer class="l-footer">
+  <div class="footer-container">
+    <div class="footer-holder">
+        <div class="l-branding">
+          <a href="#" title="Home" rel="home" class="logo"> <span class="element-invisible">Greenpeace Greenwire Nederland </span></a>
+        </div>
+    </div>
+  </div>
+</footer>

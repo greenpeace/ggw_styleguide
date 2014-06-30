c90d8a60df61113ec8d8aad348656c00c41c243b
MaikelGG <maikel.koopman@gmail.com>
2014-06-30 11:32:05 +0200

    start with dom teasers, improve action menu script, optimize imgs

--- src/js/theme.js
+++ src/js/theme.js
@@ -5615,11 +5615,7 @@ jQuery(document).ready(function ($) {
   }
 });
 
-/**
- * @file
- * Greenpeace Greenwire Core JavaScript that makes the awesomebox even more
- * awesome.
- */
+// This script creates a dropdown of the action menu, when there are more then 2 buttons.
 
 jQuery(document).ready(function ($) {
 
@@ -5627,48 +5623,37 @@ jQuery(document).ready(function ($) {
 
   // Define the context we will be operating in.
   var list = '.action-menu ul';
-  var break_width = 55;
 
   // Act only if the more tab is not created yet.
   if (typeof $(list) != 'undefined' && $(list) != false && $(list).length > 0) {
-    if ($('.action-overflowing', list).length == 0) {
 
-      // We define the actual list width here.
-      var list_width = Number($(list).css('width').replace(/[^-\d\.]/g, ''));
+    // We will store any items here that we want to move.
+    var items_to_move = new Array();
 
-      // This is the width of the 'more' tab we might be using.
-      var listitems_width = 65;
+    // Loop through all items and retrieve the index
+    $.each($('li', list), function(index, value) {
 
-      // We will store any items here that we want to move.
-      var items_to_move = new Array();
-
-      // Foreach quicktab we want to find out if it exceeds the max width.
-      $.each($('li', list), function(index, value) {
-        var item_width = Number($(value).outerWidth(true));
-        listitems_width = listitems_width + item_width;
-
-        // Max width exceeded? Add it to the items_to_move array.
-        if (listitems_width >= list_width) {
-          items_to_move[index] = value;
-        }
-      });
-
-      // If there are items to move we proceed here.
-      if (items_to_move.length > 0) {
-        // Final check to see if we actually have to do something.
-        if ((listitems_width - break_width) >= list_width) {
-          // Add our new container div.
-          $(list).append('<li class="action-overflowing"><a data-dropdown="#dropdown-action-menu" class="action-overflowing-trigger" href="#"><i class="icon icon-menu"></i> More </a><div id="dropdown-action-menu" class="dropdown dropdown-scroll"><ul class="dropdown-menu"></ul></li>');
-
-          // Foreach items that need to moved place them in the newly created
-          // awesomebox dropdown.
-          $.each(items_to_move, function(index, value) {
-            $('#dropdown-action-menu ul', list).append(value);
-          });
-        }
+      // Add everything except the first item to the items_to_move array.
+      if (index > 0) {
+        items_to_move.push(value);
       }
+   });
 
+    // If there is more than one item to move we proceed here.
+    if (items_to_move.length > 1) {
+    // Add our new container div.
+      $(list).append('<li class="action-overflowing"><a data-dropdown="#dropdown-action-menu" class="action-overflowing-trigger" href="#"><i class="icon icon-menu"></i> More </a><div id="dropdown-action-menu" class="dropdown dropdown-scroll"><ul class="dropdown-menu"></ul></li>');
+
+        // Foreach items that need to moved place them in the newly created awesomebox dropdown.
+        $.each(items_to_move, function(index, value) {
+          $('#dropdown-action-menu ul', list).append(value);
+          $('#dropdown-action-menu').hide();
+        });
     }
+
   }
 
 });
+
+
+
---
+++ src/teasers.php
@@ -0,0 +1,197 @@
+<?php include('modules/head.php'); ?>
+<body class="not-front not-logged-in page-type-news l-has-sidebar">
+
+<noscript><div id="javascript-disabled">Your JavaScript seems to be disabled, this might affect your experience on Greenpeace Greenwire.</div></noscript>
+
+<div id="outer-wrap">
+  <div id="inner-wrap">
+    <div class="layout-base">
+
+<?php include('modules/header.php'); ?>
+
+<div class="action-menu">
+  <ul>
+    <li><a href="#"><i class="icon icon-user"></i> Login</a></li>
+    <li><a href="#"><i class="icon icon-user-add"></i> Register</a></li>
+  </ul>
+</div>
+
+<div id="main" class="l-main container">
+
+  <nav class="breadcrumb" role="navigation">
+
+    <h2 class="element-invisible">You are here</h2>
+
+    <ol class="breadcrumbs"> <?php // add extra class here ?>
+      <li class="breadcrumb-item">You are here:</li>
+      <li class="breadcrumb-item">
+        <a class="breadcrumb-link" href="#">Home</a> <?php // add extra class here ?>
+      </li>
+      <li class="breadcrumb-item">
+        <a class="breadcrumb-link" href="#">News</a> <?php // add extra class here ?>
+      </li>
+      <li class="breadcrumb-item">Random news article</li>
+    </ol>
+
+  </nav>
+
+  <div id="content" class="l-main-column">
+
+    <div class="main-content">
+
+      <i class="block-icon icon-bg-news-media icon-news"></i>
+      <h1 id="page-title" class="block-title">Random news article titele which a kind of long for a title so we can see how it streches over multiple lines</h1>  <?php // add extra class here ?>
+
+      <div class="content">
+
+        <div class="views-row"> <!-- NEWS -->
+
+          <article class="node-teaser-news node-teaser">
+
+            <div class="field-type-image">
+              <picture>
+                <source media="(min-width: 48em)" srcset="http://placehold.it/130x90, http://placehold.it/260x180 2x">
+                <img src="http://placehold.it/90x60" srcset="http://placehold.it/180x120 2x" alt="The president giving an award.">
+              </picture>
+            </div>
+
+            <div class="field-type-wrapper">
+
+              <h2 class="node-title">This is an example of a long news title</h2>
+
+              <div class="submitted">
+                <a href="#" class="news">News</a> by <a href="#">Ana Hristova</a>, 23rd July 2014
+              </div>
+
+              <div class="field-name-body">
+                On touch devices, a click event has a 300ms delay before firing. The reason for this the delay  is that browsers need that buffer to make sure you aren’t going to double-tap on anything.
+              </div>
+
+            </div>
+
+          </article>
+
+        </div>
+
+
+
+        <div class="views-row"> <!-- Event -->
+
+          <article class="node-teaser-event node-teaser">
+
+            <div class="field-type-image">
+              <picture>
+                <source media="(min-width: 48em)" srcset="http://placehold.it/130x90, http://placehold.it/260x180 2x">
+                <img src="http://placehold.it/90x60" srcset="http://placehold.it/180x120 2x" alt="The president giving an award.">
+              </picture>
+            </div>
+
+            <div class="field-type-wrapper">
+
+              <h2 class="node-title">This is an example of a long event title</h2>
+
+              <div class="submitted">
+                <a href="#" class="event">Event</a>, 23rd July 2014
+              </div>
+
+              <div class="field-group-format">
+
+                <div class="field-type-date"> <label>Time:</label>August 10th, 8:00PM until 11:30 PM</div>
+                <div class="field-type-location"> <label> Address</label>Oude Markt 9B, Enschede (Market Square )</div>
+
+              </div>
+
+            </div>
+
+          </article>
+
+        </div>
+
+
+        <div class="views-row"> <!-- Group -->
+
+          <article class="node-teaser-group node-teaser">
+
+            <div class="field-type-image">
+              <picture>
+                <source media="(min-width: 48em)" srcset="http://placehold.it/130x90, http://placehold.it/260x180 2x">
+                <img src="http://placehold.it/90x60" srcset="http://placehold.it/180x120 2x" alt="The president giving an award.">
+              </picture>
+            </div>
+
+            <div class="field-type-wrapper">
+
+              <h2 class="node-title">This is an example of a long group title</h2>
+
+              <div class="submitted">
+                <a href="#" class="group">Group</a>, 3rd October 2013
+              </div>
+
+              <div class="field-group-format">
+
+                <div class="field-type-taxonomy-term-reference"> <label>Group type:</label>Campaign group</div>
+
+              </div>
+
+            </div>
+
+          </article>
+
+        </div>
+
+
+        <div class="views-row"> <!-- Album -->
+
+          <article class="node-teaser-album node-teaser">
+
+            <div class="field-type-image">
+              <picture>
+                <source media="(min-width: 48em)" srcset="http://placehold.it/130x90, http://placehold.it/260x180 2x">
+                <img src="http://placehold.it/90x60" srcset="http://placehold.it/180x120 2x" alt="The president giving an award.">
+              </picture>
+            </div>
+
+            <div class="field-type-wrapper">
+
+              <h2 class="node-title">This is an example of a long photo album title</h2>
+
+              <div class="submitted">
+                <a href="#" class="album">Photos</a> by <a href="#">Ana Hristova</a>, 23rd July 2014
+              </div>
+
+              <div class="field-name-body">
+                On touch devices, a click event has a 300ms delay before firing. The reason for this the delay  is that browsers need that buffer to make sure you aren’t going to double-tap on anything.
+              </div>
+
+            </div>
+
+          </article>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+    <aside class="l-sidebar sidebar">
+
+    <a data-replace="modules/an-article-sidebar" <?php echo ($detect->isMobile() ? "data-media='(min-width: 900px)'" : ""); ?>><span class="element-invisible">
+    Register instructions</span></a>
+
+  </aside>
+
+</div>
+
+<footer class="l-footer footer">
+  <a data-replace="modules/footer" data-media="(min-width: 900px)"><span class="element-invisible">
+    Footer</span></a>
+</footer>
+
+<!-- put js here -->
+    </div>
+  </div>
+</div>
+</body>
+</html>

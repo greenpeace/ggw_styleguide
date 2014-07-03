9c8cb89dc8c18144d39c9f55340a6c3b5522ee52
jlalovi <jlalovi@gmail.com>
2014-07-03 11:18:07 +0200

    GREEN-1862 Search styling with icon

--- src/modules/search.php
+++ src/modules/search.php
@@ -1,6 +1,7 @@
 <form action="#" method="post" class="search-form" accept-charset="UTF-8">
   <div class="form-item-search">
-     <input class="form-text" name="search" type="text" size="60" maxlength="128" placeholder="Search..." />
+     <i class="icon-search"></i>
+     <input class="form-text" name="search" type="text" size="60" maxlength="128" placeholder="Search Greenwire" />
   </div>
   <div class="form-actions">
     <input class="form-submit" type="submit" name="op" value="Search" />

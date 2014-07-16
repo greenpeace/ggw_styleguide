5588fbeeb729114f333fd0a681deec3f8b57d8a2
jlalovi <jlalovi@gmail.com>
2014-07-16 15:17:53 +0200

    GREEN-1861 Styleguide: Components. Teassers

--- src/modules/teasers/users.php
+++ src/modules/teasers/users.php
@@ -1,24 +1,24 @@
 <article class="node-teaser-user node-teaser">
 
-            <div class="field-type-image">
-              <picture>
-                <source media="(min-width: 48em)" srcset="http://placehold.it/130x90, http://placehold.it/260x180 2x">
-                <img src="http://placehold.it/90x60" srcset="http://placehold.it/180x120 2x" alt="The president giving an award.">
-              </picture>
-            </div>
+  <div class="field-type-image">
+    <picture>
+      <source media="(min-width: 48em)" srcset="http://placehold.it/130x90, http://placehold.it/260x180 2x">
+      <img src="http://placehold.it/90x60" srcset="http://placehold.it/180x120 2x" alt="The president giving an award.">
+    </picture>
+  </div>
 
-            <div class="field-type-wrapper">
+  <div class="field-type-wrapper">
 
-              <h2 class="node-title">This is an example of a long photo album title</h2>
+    <h2 class="node-title">This is an example of a long photo album title</h2>
 
-              <div class="submitted">
-                <a href="#" class="user">User</a> by <a href="#">Ana Hristova</a>, 23rd July 2014
-              </div>
+    <div class="submitted">
+      <a href="#" class="user">User</a> by <a href="#">Ana Hristova</a>, 23rd July 2014
+    </div>
 
-              <div class="field-name-body">
-                On touch devices, a click event has a 300ms delay before firing. The reason for this the delay  is that browsers need that buffer to make sure you aren’t going to double-tap on anything.
-              </div>
+    <div class="field-name-body">
+      On touch devices, a click event has a 300ms delay before firing. The reason for this the delay  is that browsers need that buffer to make sure you aren’t going to double-tap on anything.
+    </div>
 
-            </div>
+  </div>
 
-          </article>
+</article>

5b15936c161c784341f19943e94c124c4052820d
MaikelGG <maikel.koopman@gmail.com>
2014-07-23 15:11:32 +0200

    replace local img with public

--- src/modules/user-menu-LU.php
+++ src/modules/user-menu-LU.php
@@ -1,4 +1,4 @@
 <ul class="menu">
-  <li class="menu-item"><a href="#" class="menu-link"><img src="http://ggw.dev/sites/default/files/styles/tiny_avatar/public/11a25e87-f069-406f-ada9-3b1d9a98fa57.jpg"> Ana Hristova <i class="icon-comment"></i><span class="num-notifications">4</span></a></li>
+  <li class="menu-item"><a href="#" class="menu-link"><img src="http://placekitten.com/20/20"> Ana Hristova <i class="icon-comment"></i><span class="num-notifications">4</span></a></li>
   <li class="menu-item"><a href="#" class="menu-link">Log out</a></li>
 </ul>

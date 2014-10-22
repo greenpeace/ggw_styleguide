19de686f59620849c2f5c34b9176208dfc29497c
MaikelGG <maikel.koopman@gmail.com>
2014-10-22 14:32:56 +0200

    fixed teasers, search action bar

--- source/js/contrib/jquery.dropdown.js
+++ source/js/contrib/jquery.dropdown.js
@@ -103,14 +103,26 @@ if(jQuery) (function($) {
 
         if( dropdown.length === 0 || !trigger ) return;
 
-        /* Position the dropdown
-        dropdown
-            .css({
-                left: dropdown.hasClass('dropdown-anchor-right') ?
-                    trigger.offset().left - (dropdown.outerWidth() - trigger.outerWidth()) + hOffset : trigger.offset().left + hOffset,
-                top: trigger.offset().top + trigger.outerHeight() + vOffset
+        // Exclude off-canvas nav elements from this function
+        if (!(dropdown).hasClass("relative")) {
+
+          // Position the dropdown relative-to-parent...
+          if (dropdown.hasClass('dropdown-relative')) {
+            dropdown.css({
+              left: dropdown.hasClass('dropdown-anchor-right') ?
+                trigger.position().left - (dropdown.outerWidth(true) - trigger.outerWidth(true)) - parseInt(trigger.css('margin-right'), 10) + hOffset :
+                trigger.position().left + parseInt(trigger.css('margin-left'), 10) + hOffset,
+              top: trigger.position().top + trigger.outerHeight(true) - parseInt(trigger.css('margin-top'), 10) + vOffset
             });
-        */
+          } else {
+            // ...or relative to document
+            dropdown.css({
+              left: dropdown.hasClass('dropdown-anchor-right') ?
+                trigger.offset().left - (dropdown.outerWidth() - trigger.outerWidth()) + hOffset : trigger.offset().left + hOffset,
+              top: trigger.offset().top + trigger.outerHeight() + vOffset
+            });
+          }
+        }
     }
 
     $(document).on('click.dropdown', '[data-dropdown]', show);

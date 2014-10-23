a610a6795f649a4457b8aae140e7d7decda88a6b
MaikelGG <maikel.koopman@gmail.com>
2014-10-23 17:46:04 +0200

    list view for members with table nav

--- source/js/custom/9-runScripts.js
+++ source/js/custom/9-runScripts.js
@@ -126,7 +126,7 @@ $( window ).load(function() {
     }
   });
 
-  // add class to external links so they can be styled
+   // add class to external links so they can be styled
   $('a').filter(function() {
     return this.hostname && this.hostname !== location.hostname;
   }).attr('target', '_blank').addClass('external');

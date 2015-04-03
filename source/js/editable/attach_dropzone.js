a52c801accfbb017cda2ec9ae7eb80d51ffc3b86
Maikel Koopman <maikelkoopman@goalgorilla.com>
2015-04-03 16:27:32 +0200

    create idea, create news, create video, create material, video thumbnail preview

--- source/js/editable/attach_dropzone.js
+++ source/js/editable/attach_dropzone.js
@@ -140,8 +140,88 @@
 })(jQuery);
 
 $(function() {
+
   $('#previews').sortable({
     handle: '.preview',
     items: ':not(.disabled)'
   });
+
+  var previewNode = $("#template");
+  previewNode.removeAttr('id');
+  var previewTemplate = previewNode.parent().html();
+  $(previewNode).remove();
+
+  if( $("body").hasClass("page-type-photo") ){
+    var myDropzone = new Dropzone(document.body, {
+      url: "http://www.torrentplease.com/dropzone.php",
+      thumbnailWidth: 80,
+      thumbnailHeight: 80,
+      parallelUploads: 20,
+      acceptedFiles: "image/*",
+      previewTemplate: previewTemplate,
+      autoQueue: false, // Make sure the files aren't queued until manually added
+      previewsContainer: "#previews", // Define the container to display the previews
+     clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+    });
+
+  } else if ( $("body").hasClass("page-type-document") ) {
+
+    var myDropzone = new Dropzone(document.body, {
+      url: "http://www.torrentplease.com/dropzone.php",
+      thumbnailWidth: 80,
+      thumbnailHeight: 80,
+      parallelUploads: 20,
+      acceptedFiles: ".doc, .docx, .xls, .xlsx, .ppt, .pptx, .odt, .odp, .ods, .pdf",
+      previewTemplate: previewTemplate,
+      autoQueue: false, // Make sure the files aren't queued until manually added
+      previewsContainer: "#previews", // Define the container to display the previews
+      clickable: ".fileinput-button" // Define the element that should be used as click trigger to select files.
+    });
+
+    myDropzone.on("addedfile", function(file) {
+      var ext = file.name.split('.').pop();
+      file.previewElement.querySelector(".image-preview").className += " " + ext;
+      file.previewElement.querySelector(".image-preview").innerHTML = "";
+    });
+
+  }
+
+  myDropzone.on("addedfile", function(file) {
+    $('#previews').sortable('reload');
+    $('.dropzone-droparea').addClass('dz-started');
+
+    file.previewElement.querySelector(".start").onclick = function() {
+      myDropzone.enqueueFile(file);
+    };
+  });
+
+  // Update the total progress bar
+  myDropzone.on("totaluploadprogress", function(progress) {
+    $("#total-progress .progress-bar").css('width', progress + '%');
+  });
+
+  myDropzone.on("sending", function(file) {
+    // Show the total progress bar when upload starts
+    $("#total-progress").css('opacity', '1');
+    // And disable the start button
+    file.previewElement.querySelector(".start").setAttribute("disabled", "disabled");
+  });
+
+  // Hide the total progress bar when nothing's uploading anymore
+  myDropzone.on("queuecomplete", function(progress) {
+    $("#total-progress").css('opacity', '0');
+  });
+
+  // Setup the buttons for all transfers
+  // The "add files" button doesn't need to be setup because the config
+  // `clickable` has already been specified.
+  $("#dropzone-actions .start").on('click', function(){
+    myDropzone.enqueueFiles(myDropzone.getFilesWithStatus(Dropzone.ADDED));
+    $('#previews').sortable('reload');
+  });
+  $("#dropzone-actions .cancel").on('click', function(){
+    myDropzone.removeAllFiles(true);
+    $('.dropzone-droparea').removeClass('dz-started');
+  });
+
 });

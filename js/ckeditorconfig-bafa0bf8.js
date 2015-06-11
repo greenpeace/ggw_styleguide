ef2a0e5aa3bed9ac6a94982b5c5d190295f24d8b
Maikel Koopman <maikel.koopman@goalgorilla.com>
2015-06-11 15:29:51 +0200

    Automated commit at 2015-06-11 13:29:51 UTC by middleman-deploy 1.0.0

--- js/ckeditorconfig-bafa0bf8.js
+++ js/ckeditorconfig-bafa0bf8.js
@@ -1,1 +1,1 @@
-CKEDITOR.config.toolbar=[["Bold","Italic","RemoveFormat"],["NumberedList","BulletedList"],["Format"],["Image","Maximize","Link","Unlink"],["Source"]],CKEDITOR.config.contentsCss=["/css/all-6174ee78.css","/css/x-editable-3f2755bd.css"];
+CKEDITOR.config.toolbar=[["Bold","Italic","RemoveFormat"],["NumberedList","BulletedList"],["Format"],["Image","Maximize","Link","Unlink"],["Source"]],CKEDITOR.config.contentsCss=["/css/all-0cc7bb80.css","/css/x-editable-3f2755bd.css"];

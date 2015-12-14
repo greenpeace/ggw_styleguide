bc26e9b22d6b27075c7419a38b4c0300720d3720
Niels van der Molen <nielsvandermolen@goalgorilla.com>
2015-12-14 14:39:25 +0100

    Automated commit at 2015-12-14:14:39:25 by deploy script using middleman build

--- js/ckeditorconfig-89840ca0.js
+++ js/ckeditorconfig-89840ca0.js
@@ -1,1 +1,1 @@
-CKEDITOR.config.toolbar=[["Bold","Italic","RemoveFormat"],["NumberedList","BulletedList"],["Format"],["Image","Link","Unlink"],["Source"]],CKEDITOR.config.contentsCss=["/css/all-744ec35e.css","/css/x-editable-21d6640d.css"];
+CKEDITOR.config.toolbar=[["Bold","Italic","RemoveFormat"],["NumberedList","BulletedList"],["Format"],["Image","Link","Unlink"],["Source"]],CKEDITOR.config.contentsCss=["/css/all-edfa0b18.css","/css/x-editable-21d6640d.css"];

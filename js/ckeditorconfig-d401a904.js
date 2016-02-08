a45ea0f5fd1b20db29893052a23abae2862895cf
Crazylord <sergij.chornobay@lemberg.co.uk>
2016-02-08 13:49:21 +0000

    Automated commit at 2016-02-08:13:49:21 by deploy script using middleman build

--- js/ckeditorconfig-d401a904.js
+++ js/ckeditorconfig-d401a904.js
@@ -1,1 +1,1 @@
-CKEDITOR.config.toolbar=[["Bold","Italic","RemoveFormat"],["NumberedList","BulletedList"],["Scayt"],["Image","Video","Link","Unlink"],["Format"],["Source"]],CKEDITOR.config.contentsCss=["/css/style-c0a08195.css","/css/x-editable-9b2e6f78.css"];
+CKEDITOR.config.toolbar=[["Bold","Italic","RemoveFormat"],["NumberedList","BulletedList"],["Scayt"],["Image","Video","Link","Unlink"],["Format"],["Source"]],CKEDITOR.config.contentsCss=["/css/style-e0ee93de.css","/css/x-editable-9b2e6f78.css"];
